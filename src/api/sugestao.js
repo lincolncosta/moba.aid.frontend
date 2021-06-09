1bd1e2fa6952125cae3aea5b049034fb6b52ff68
tekpixo <lincoln.mcosta@gmail.com>
2021-06-09 15:33:57 -0300

    Atualização do endpoint de sugestão.

--- src/api/sugestao.js
+++ src/api/sugestao.js
@@ -1,3 +1,3 @@
 const axios = require('axios')
 
-export const getSugestao = () => axios.get(`${process.env.REACT_APP_API_URL}/lalala`).then((data) => data.data)
+export const getSugestao = () => axios.get(`${process.env.REACT_APP_API_URL}/suggest`).then((data) => data.data)
