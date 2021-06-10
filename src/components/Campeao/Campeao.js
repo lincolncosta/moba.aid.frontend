3e977ae9434783911283931900ffac1cdd0a0ed5
Ricardo Balbinot <ricardo.balbinot@hotmail.com>
2021-06-10 04:25:19 -0300

    Cria fluxo de seleção de campeões

--- src/components/Campeao/Campeao.js
+++ src/components/Campeao/Campeao.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
 import { Box } from '../Box'
 import { Text } from '../Text'
@@ -20,14 +20,20 @@ const CampeaoImage = styled(Box)`
 
   height: 75px;
   width: 75px;
+
+  ${(props) =>
+    props.selecionado &&
+    css`
+      box-shadow: 0 0 15px gold;
+    `}
 `
 
-export const Campeao = ({ nome, alias, fontSizeNome, ...props }) => {
+export const Campeao = ({ nome, alias, fontSizeNome, selecionado, ...props }) => {
   const img = `http://ddragon.leagueoflegends.com/cdn/11.10.1/img/champion/${alias}.png`
 
   return (
     <CampeaoStyled {...props}>
-      <CampeaoImage style={{ backgroundImage: `url(${img})` }} mb={3} />
+      <CampeaoImage selecionado={selecionado} style={{ backgroundImage: `url(${img})` }} mb={3} />
       <Text textAlign="center" fontSize={fontSizeNome || 12} color="textColor">
         {nome}
       </Text>
