3aaef202f96944c969dad7ab96e8a6af79956d54
Ricardo Balbinot <ricardo.balbinot@hotmail.com>
2021-06-16 03:46:26 -0300

    Otimiza display de sugestão

--- src/pages/SelecaoCampeoes/SelecaoCampeoes.js
+++ src/pages/SelecaoCampeoes/SelecaoCampeoes.js
@@ -69,6 +69,8 @@ export const SelecaoCampeoes = memo(() => {
 
       if (proximoTime === meuTime) {
         buscaCampeoesSugeridos()
+      } else {
+        setCampeoesSugeridos([])
       }
     }
   }
